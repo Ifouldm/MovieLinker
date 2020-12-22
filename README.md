8c8dc3bf154b6de92f9c8ac2177e9f397af26621
Matt Ifould <ifouldm@gmail.com>
2020-12-22 12:29:33 +0000

    Linting setup

--- README.md
+++ README.md
@@ -12,34 +12,36 @@
 
 ## How will users interact with your web site?
 
-> The user will enter 2 movie titles into seperate text input fields, these will be validated and an auto-complete will help identifying the exact title.
+> The user will enter 2 movie titles into seperate text input fields, these will be validated and an auto-complete will help identifying the exact title. Once both titles have been selected a button can be pressed which will perform the lookup and present the results on the same page.
 
 ## What 3rd Party API(s) will you integrate with?
 
-- http://www.omdbapi.com/ 
+> http://www.omdbapi.com/
+> 
 > I will use 'The Open Movie Database' API to retrieve the relevant data from the user input.
 
 ## Tech Stack
 
-- I intend to complete this project without a CSS framework
-- As far as possible I want to avoid additional helpers and libraries
+> - I intend to complete this project without a CSS framework
+> - As far as possible I want to avoid additional helpers and libraries
 
 ## Will you try to implement any stretch features?
 
-### My stretch goals
-- More than 2 movies
+> ### My stretch goals
+> - More than 2 movies
+> - An elegant reset option (change just one of the title?)
 
-### From stretch goal suggestions 
-- UI Animations
-- Themes
-- Preferences stored locally
-- Document javascript with JSDoc
-- Single page application
+> ### From stretch goal suggestions 
+> - UI Animations
+> - Themes
+> - Preferences stored locally
+> - Document javascript with JSDoc
+> - Single page application
 
 ## What will be your process?
 
-- Github will be used to manage the project, to-do list, issues etc?
-- I intend to commit code on each feature completion and refactor process.
-- The website will be built using Chrome dev tools for debugging
-- The final product will be tested on various browsers including mobile
-- Initial design will be done on whiteboard and then rapid prototyping will be used to implement
+> - Github will be used to manage the project, to-do list, issues etc?
+> - I intend to commit code on each feature completion and refactor process.
+> - The website will be built using Chrome dev tools for debugging
+> - The final product will be tested on various browsers including mobile
+> - Initial design will be done on whiteboard and then rapid prototyping will be used to implement
--- main.js
+++ main.js
@@ -1,55 +1,55 @@
 const APIKEY = 'a3ed1e37';
 
-document.getElementById("filmEntryForm").addEventListener("submit", function(event){
-    event.preventDefault();
+document.getElementById('filmEntryForm').addEventListener('submit', (event) => {
+  event.preventDefault();
 
-    const film1Element = document.getElementById('film1');
-    const film2Element = document.getElementById('film2');
+  const film1Element = document.getElementById('film1');
+  const film2Element = document.getElementById('film2');
 
-    const fetchUrl1 = `http://www.omdbapi.com/?t=${film1Element.value}&apikey=${APIKEY}`;
-    const fetchUrl2 = `http://www.omdbapi.com/?t=${film2Element.value}&apikey=${APIKEY}`;
+  const fetchUrl1 = `http://www.omdbapi.com/?t=${film1Element.value}&apikey=${APIKEY}`;
+  const fetchUrl2 = `http://www.omdbapi.com/?t=${film2Element.value}&apikey=${APIKEY}`;
 
-    fetch('film1.json')
-        .then(response => response.json())
-        .then(data => console.log(data));
+  fetch('film1.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
-    fetch('film2.json')
-        .then(response => response.json())
-        .then(data => console.log(data));
+  fetch('film2.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
-    displayResults([
-        {
-            title: 'Director',
-            members: [
-                'James Gunn'
-            ]
-        },
-        {
-            title: 'Cast',
-            members: [
-                'Chris Pratt',
-                'Zoe Saldana'
-            ]
+  displayResults([
+    {
+      title: 'Director',
+      members: [
+        'James Gunn',
+      ],
+    },
+    {
+      title: 'Cast',
+      members: [
+        'Chris Pratt',
+        'Zoe Saldana',
+      ],
     }]);
 });
 
 function getCommonElements() {
-    
+
 }
 
 function displayResults(results) {
-    const resultsElement = document.getElementById('results');
+  const resultsElement = document.getElementById('results');
 
-    results.forEach(category => {
-        const headingElement = document.createElement('h3');
-        const list = document.createElement('ul');
-        headingElement.textContent = category.title;
-        category.members.forEach(name => {
-            const listElement = document.createElement('li');
-            listElement.textContent = name;
-            list.appendChild(listElement);
-        })
-        resultsElement.appendChild(headingElement);
-        resultsElement.appendChild(list);
+  results.forEach((category) => {
+    const headingElement = document.createElement('h3');
+    const list = document.createElement('ul');
+    headingElement.textContent = category.title;
+    category.members.forEach((name) => {
+      const listElement = document.createElement('li');
+      listElement.textContent = name;
+      list.appendChild(listElement);
     });
+    resultsElement.appendChild(headingElement);
+    resultsElement.appendChild(list);
+  });
 }
